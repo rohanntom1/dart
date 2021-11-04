e7b6d28aeb70c80f358c2c25265a19b9c0626647
rohanntom1 <93723865+rohanntom1@users.noreply.github.com>
2021-11-04 21:00:56 +0530

    Add files via upload

---
+++ store/bin/store.dart
@@ -0,0 +1,30 @@
+import 'invoice.dart';
+import 'dart:io';
+
+void main() {
+  int input;
+  do {
+    print('Store Management');
+    print('1. Create a New Invoice');
+    print('2. View Total Sale');
+    print('3. QUIT');
+    stdout.write('Enter your choice: ');
+    input = int.parse(stdin.readLineSync()!);
+    if (input == 1) {
+      final invoice = new Invoice();
+      return invoice.billing();
+      //revenue= revenue+;
+
+      // print(item);
+    } else if (input == 2) {
+      final sales = new Invoice();
+      //double sale = sales.totalSale(revenue);
+    } else if (input == 3) {
+      //quit
+      print('**Quitting**');
+      break;
+    } else {
+      throw Exception('Enter a valid choice');
+    }
+  } while (true);
+}
